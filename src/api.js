7fc632b318aa1d3160492b02b99a873401a501c7
Sergi <sergialsina81@gmail.com>
2022-12-02 14:03:16 +0100

    kata6resuelta

---
+++ src/api.js
@@ -0,0 +1,14 @@
+document.querySelector('button').addEventListener('click',()=>{
+
+  let myParrafo = document.querySelector('p')
+
+  fetch('https://uselessfacts.jsph.pl/random.json?language=en')
+  .then(result => result.json())
+  .then(data => {
+    myParrafo.textContent = data.text
+  })
+  .catch(error => console.log(error))
+
+})
+
+
